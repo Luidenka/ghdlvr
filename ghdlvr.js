6a8a3a8950c405753cbce4e0668d0c544668caa9
LSC-1G-Class-Monthly <jarvislui1128@gmail.com>
2022-11-09 13:48:30 +0800

    Simplify code

--- ghdlvr.js
+++ ghdlvr.js
@@ -1,33 +1,4 @@
-var link = new URL(window.location.href);
-var file = link.searchParams.get('f');
-
-if (file == null) {
-    document.write('\<!DOCTYPE html>\
-    \
-    <head>\
-        <title>GHDlvr</title>\
-    </head>\
-    \
-    <body>\
-        <h1>GHDlvr - the new CDN for GitHub Repos</h1>\
-        <form>\
-            <input placeholder="Enter GitHub file link" id="ghlink">\
-            <input disabled placeholder="GHDlvr link output here" id="output">\
-        </form>\
-        <script>\
-            document.getElementById(\'ghlink\').onchange = (e) => {\
-                document.getElementById(\'output\').value =\
-                    `https://ghdlvr.github.io?f=${new URL(document.getElementById(\'ghlink\').value).pathname}`\
-            }\
-        </script>\
-    </body>')
-}
-else {
-    var code = document.createElement('pre');
-    code.style = "word-wrap: break-word";
-    code.style.whiteSpace = "pre-wrap";
-    document.head.appendChild(meta);
-    document.body.appendChild(code);
-    fetch(`https://raw.githubusercontent.com${file}`)
-        .then(response => response.text().then(data => code.innerText = data));
-}
+var code = document.createElement('pre');
+document.body.appendChild(code);
+fetch(`https://raw.githubusercontent.com${file}`)
+    .then(response => response.text().then(data => code.innerText = data));
